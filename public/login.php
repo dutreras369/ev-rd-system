43c8d1ba23791dfe2e6ce7521fa98a6a074942d2
dutreras <dutreras369@gmail.com>
2024-12-29 11:54:10 -0300

    notificacion

--- public/login.php
+++ public/login.php
@@ -3,6 +3,21 @@ $title = "Inicio de Sesión";
 
 require_once __DIR__ . '/../views/templates/header.php';
 
+?>
+
+
+<div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+    <h4 class="alert-heading">¡Importante!</h4>
+    <p>Hemos actualizado nuestro enlace de Telegram. Por favor, accede al nuevo grupo para mantenerte informado.</p>
+    <a href="https://t.me/nuevo_enlace_telegram" class="btn btn-primary btn-lg mt-3" target="_blank">
+        <i class="bi bi-telegram"></i> Acceder a Telegram
+    </a>
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+<?php
+
 require_once __DIR__ . '/../views/auth/login.php';
 
 require_once __DIR__ . '/../views/templates/footer.php';
+
