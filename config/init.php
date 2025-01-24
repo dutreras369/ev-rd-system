82520c095ec1052c14c4782ecfdc70cf33585289
dutreras <dutreras369@gmail.com>
2025-01-24 00:15:21 -0300

    update security impl 0.3

--- config/init.php
+++ config/init.php
@@ -11,3 +11,7 @@ require_once __DIR__ . '/Database.php';
 // Log de inicialización
 require_once __DIR__ . '/../helpers/Logger.php';
 
+// Iniciañizar sesion
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
