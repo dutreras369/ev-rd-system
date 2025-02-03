f5e9a03a3c2a48ee200a4722d75caa51e99b35a5
dutreras <dutreras369@gmail.com>
2025-02-03 01:39:10 -0300

    update record 0.2

--- routes/record.php
+++ routes/record.php
@@ -20,25 +20,20 @@ switch ($method) {
         }
         break;
 
-        case 'register':
-            if ($method !== 'POST') {
-                http_response_code(405);
-                echo json_encode(['success' => false, 'error' => 'Método HTTP no permitido.']);
-                exit;
-            }
-        
-            $inputData = json_decode(file_get_contents("php://input"), true);
-            
-            if (!$inputData) {
+        case 'POST':
+            if ($action === 'register') {
+                $data = json_decode(file_get_contents("php://input"), true);
+                if (isset($data['user_id'], $data['codigo_usuario'], $data['token'], $data['movement_type'], $data['amount'], $data['timestamp'])) {
+                    echo json_encode($recordController->createRecord($data));
+                } else {
+                    http_response_code(400);
+                    echo json_encode(['success' => false, 'error' => 'Datos incompletos.']);
+                }
+            } else {
                 http_response_code(400);
-                echo json_encode(['success' => false, 'error' => 'Datos inválidos.']);
-                exit;
+                echo json_encode(['success' => false, 'error' => 'Acción no válida']);
             }
-        
-            $response = $recordController->createRecord($inputData);
-            echo json_encode($response);
-            break;        
-        
+            break;
 
     default:
         http_response_code(405);
