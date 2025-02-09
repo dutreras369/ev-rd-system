f025434ff6804c262ce5829d1ca63f983a3d4063
dutreras <dutreras369@gmail.com>
2025-02-09 05:05:43 -0300

    add records functions for adminimpl total records 0.1

--- routes/analysis.php
+++ routes/analysis.php
@@ -10,21 +10,34 @@ $method = $_SERVER['REQUEST_METHOD'];
 $action = $_GET['action'] ?? null;
 
 try {
-    if ($method === 'POST') {
-        $data = json_decode(file_get_contents("php://input"), true);
+    switch ($method) {
+        /*case 'GET':
+            if ($action === 'details') {
+                if (!isset($_GET['user_id'], $_GET['token'], $_GET['record_id'])) {
+                    throw new Exception('Datos incompletos.', 400);
+                }
 
-        if ($action === 'total_records') {
-            if (!isset($data['user_id'], $data['token'])) {
-                throw new Exception('Datos incompletos.', 400);
+                $response = $recordController->getRecordDetails($_GET['record_id']);
+                echo json_encode($response);
+            } else {
+                throw new Exception('Acción no válida para GET', 400);
             }
+            break; */
 
-            $response = $analysisController->getTotalStatusRecords();
-            echo json_encode($response);
-        } else {
-            throw new Exception('Acción no válida para POST', 400);
-        }
-    } else {
-        throw new Exception('Método HTTP no permitido', 405);
+        case 'POST':
+            $data = json_decode(file_get_contents("php://input"), true);
+
+            if ($action === 'total_records') {
+                if (!isset($data['user_id'], $data['token'])) {
+                    throw new Exception('Datos incompletos.', 400);
+                }
+
+                $response = $analysisController->getTotalStatusRecords();
+                echo json_encode($response);
+
+            }
+        default:
+            throw new Exception('Método HTTP no permitido', 405);
     }
 } catch (Exception $e) {
     http_response_code($e->getCode() ?: 500);
